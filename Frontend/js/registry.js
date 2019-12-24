49d45b08afd64a61be6cf177e49261c0200b7bf0
Denis <47882549+sncodeGit@users.noreply.github.com>
2019-12-24 03:33:18 +0300

    Create registry.js

---
+++ Frontend/js/registry.js
@@ -0,0 +1,14 @@
+function checkRegistryData() {
+    var data = new Array(4);
+    data[0] = document.getElementsByName("email")[0].value;
+    data[1] = document.getElementsByName("login")[0].value;
+    data[2] = document.getElementsByName("first_password")[0].value;
+    data[3] = document.getElementsByName("second_password")[0].value;
+    
+    if (data[0] == "" || data[1] == "" || data[2] == "" || data[3] == ""){
+        alert("Одно из полей пустое. Попробуйте снова.");
+    }
+    else if (data[2] != data[3]){
+        alert("Пароли не совпадают. Попробуйте снова.");
+    }
+}
