f3156b16ff4a23344172c2a398de281da9f51dd6
Denis <47882549+sncodeGit@users.noreply.github.com>
2019-12-26 17:37:43 +0300

    Update registry.js

--- Frontend/js/registry.js
+++ Frontend/js/registry.js
@@ -5,10 +5,15 @@ function checkRegistryData() {
     data[2] = document.getElementsByName("first_password")[0].value;
     data[3] = document.getElementsByName("second_password")[0].value;
     
+    var valid = true;
     if (data[0] == "" || data[1] == "" || data[2] == "" || data[3] == ""){
         alert("Одно из полей пустое. Попробуйте снова.");
+        valid = false;
     }
     else if (data[2] != data[3]){
         alert("Пароли не совпадают. Попробуйте снова.");
+        valid = false;
     }
+    
+    return valid;
 }
