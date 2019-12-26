6a50fe578e0833a25e6313489698ded55be2b70d
Denis <47882549+sncodeGit@users.noreply.github.com>
2019-12-26 19:19:08 +0300

    Update lambda_function.py

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -3,30 +3,28 @@ import sys
 import logging
 import pymysql
 import support_db_func as dbfunc
+import config
+import css
 
 logger = logging.getLogger() 
 logger.setLevel(logging.INFO)
 ret = {}
+html_param = {}
 
 ret['statusCode'] = 404
 ret['headers'] = {
     'Content-Type': 'text/html'
 }
-ret['body'] = f'''
-        <html>
-        <head>
-        <meta charset="UTF-8">
-        <title>Не найдено</title>
-        </head>
-        <body>
-            Мы не нашли запрошенного вами документа :( <br>
-            Пожалуйста, попробуйте вернуться назад
-        </body>
-        </html>
-        '''
+
+html_param['title'] = "Не найдено"
+html_param['text'] = '''
+        Мы не нашли запрошенного вами документа :( <br><br>
+        Пожалуйста, попробуйте вернуться назад или перейдите на <a href="%s%s">главную</a>.
+        ''' % (config.site_protocol, config.site_url)
 
 def lambda_handler(event, context):
     main(event)
+    html_formatting()
     return ret
     
 def main(event):
@@ -48,17 +46,11 @@ def registry(data):
         conn.commit()
         
     ret['statusCode'] = 200
-    ret['body'] = f'''
-            <html>
-            <head>
-            <meta charset="UTF-8">
-            <title>Завершение регистрации</title>
-            </head>
-            <body>
-                Благодарим за регистрацию!
-            </body>
-            </html>
-            '''
+    html_param['title'] = "Завершение регистрации"
+    html_param['text'] = '''
+            Благодарим за регистрацию! <br><br>
+            Переходите на страницу <a href="%s%s">авторизации</a>.
+            ''' % (config.site_protocol, config.site_url)
     return
     
 def auth(data):
@@ -75,30 +67,33 @@ def auth(data):
     
     if (real_pass == data['password']):
         ret['statusCode'] = 200
-        ret['body'] = f'''
-            <html>
-            <head>
-            <meta charset="UTF-8">
-            <title>Внутренняя часть сайта</title>
-            </head>
-            <body>
+        html_param['title'] = "Внутренняя часть сайта"
+        html_param['text'] = '''
                 Вы успешно авторизованы. Поздравляем!
-            </body>
-            </html>
             '''
     else:
         ret['statusCode'] = 401
-        ret['body'] = f'''
-            <html>
-            <head>
-            <meta charset="UTF-8">
-            <title>Неавторизованный запрос</title>
-            </head>
-            <body>
-                Логин или пароль введены некорректно.<br>
-                Также возможно, что вы ещё не зарегистрировались.<br>
-                Тогда рекомендуем перейти на страницу регистрации и сделать это!
-            </body>
-            </html>
-            '''
+        html_param['title'] = "Неавторизованный запрос"
+        html_param['text'] = '''
+                Логин или пароль введены некорректно
+                или вы просто ещё не зарегистрированы.<br><br>
+                Рекомендуем попробовать <a href="%s%s">авторизоваться снова</a>
+                или перейти на <a href="%s%s/registry.html">страницу регистрации</a>.
+            ''' % (config.site_protocol, config.site_url, config.site_protocol, config.site_url)
     return
+
+def html_formatting():
+    ret['body'] = f'''
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        %s
+        <title>%s</title>
+        </head>
+        <body>
+            <div class="form">
+                %s
+            </div>
+        </body>
+        </html>
+        ''' % (css.auth_css, html_param['title'], html_param['text'])
